a45e0628495bbc94245237f4316052a4e68c9fbe
Prakanshu <prakanshusrivastava3677@gmail.com>
2025-04-14 16:14:39 +0530

    Java codes for automation learning

---
+++ src/AgeCalculator.java
@@ -0,0 +1,18 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class AgeCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the User's age: ");
+        int age = sc.nextInt();
+
+        if (age<18){
+            System.out.println("User is MINOR ");
+        } else if (age <= 59) {
+            System.out.println("User is ADULT ");
+        }else{
+            System.out.println("User is SENIOR ");
+        }
+
+    }
+}
---
+++ src/AreaofRectangle.java
@@ -0,0 +1,17 @@
+public class AreaofRectangle {
+    public static void main(String[] args) {
+        // input
+        int length = 5;
+        int width = 3;
+
+        // Area and perimeter of rectangle
+        int area = length * width;
+        int perimeter = 2 * (length + width);
+
+        // Print outpur
+        System.out.println("Length of Rectangle is : " + length);
+        System.out.println("Width of Rectangle is : " + width);
+        System.out.println("Area of Rectangle is : " + area);
+        System.out.println("Perimeter of Rectangle is : " + perimeter);
+    }
+}
