51210cc06f51f351b182908758caeaeb67da56f3
Prakanshu Srivastava <42319499+prakanshu08@users.noreply.github.com>
2025-04-14 16:09:19 +0530

    Add files via upload
    Day 1 Assignment

---
+++ Dtype.java
@@ -0,0 +1,23 @@
+public class Dtype {
+    public static void main(String[] args) {
+        //initialize int var
+        int i = 5;
+        System.out.println("integer is: " + i);
+
+        //initialize double var
+        double d = 3.14567;
+        System.out.println("Double is: " + d);
+
+        //initialize char type var
+        char c = 'J';
+        System.out.println("Character is: " + c);
+
+        //initialize boolean var
+        boolean fanOff = false;
+        System.out.println("Boolean is:" +fanOff);
+
+        //initialize String var
+        String s = "Hello Automation";
+        System.out.println("String is :" + s);
+    }
+}
